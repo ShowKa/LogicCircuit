e52b709a80d05ddc2e92e5112a64e70ee54eaab0
John Doe <johndoe@example.com>
2020-02-06 18:40:25 +0900

    install sass(scss) + Bootstrap

--- build/webpack.config.dev.js
+++ build/webpack.config.dev.js
@@ -4,6 +4,7 @@ const {
   VueLoaderPlugin
 } = require('vue-loader')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 module.exports = {
   mode: 'development',
@@ -27,6 +28,43 @@ module.exports = {
           'vue-style-loader',
           'css-loader'
         ]
+      },
+      {
+        // 対象となるファイルの拡張子(scss)
+        test: /\.scss$/,
+        // Sassファイルの読み込みとコンパイル
+        use: ExtractTextPlugin.extract([
+          // CSSをバンドルするための機能
+          {
+            loader: 'css-loader',
+            options: {
+              // オプションでCSS内のurl()メソッドの取り込まない
+              url: false,
+              // ソースマップの利用有無
+              sourceMap: true,
+              // Sass+PostCSSの場合は2を指定
+              importLoaders: 2
+            },
+          },
+          // PostCSSのための設定
+          {
+            loader: 'postcss-loader',
+            options: {
+              // PostCSS側でもソースマップを有効にする
+              sourceMap: true,
+              // ベンダープレフィックスを自動付与する
+              plugins: () => [require('autoprefixer')]
+            },
+          },
+          // Sassをバンドルするための機能
+          {
+            loader: 'sass-loader',
+            options: {
+              // ソースマップの利用有無
+              sourceMap: true,
+            }
+          }
+        ])
       }
     ]
   },
@@ -37,6 +75,10 @@ module.exports = {
       filename: 'index.html',
       template: 'index.html',
       inject: true
-    })
-  ]
+    }),
+    // js にバンドルした css を .css ファイルに抽出しなおす。
+    new ExtractTextPlugin('style.css')
+  ],
+  // source-map方式でないと、CSSの元ソースが追跡できないため
+  devtool: "source-map"
 }
--- src/app.js
+++ src/app.js
@@ -1,6 +1,10 @@
 import Vue from 'vue'
 import App from './App.vue'
 import '../assets/app.css'
+// BootstrapのJavaScript側の機能
+import "bootstrap";
+// スタイルシート
+import "./index.scss";
 new Vue({
   el: '#app',
   render: h => h(App)
