3bfa50dd332b2ea74f31147a630841e38a580c3a
John Doe <johndoe@example.com>
2020-02-15 21:34:19 +0900

    Design Element For LogicCircuit

---
+++ src/components/element/Element.vue
@@ -0,0 +1,65 @@
+<template>
+  <div class="element">
+    <div class="element__frame">
+      <div>
+        <div class="element__input element__input--border-bottom" />
+        <div class="element__input" />
+      </div>
+      <div class="element__center">
+        {{ elementType }}
+      </div>
+      <div class="element__output" />
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  props: {
+    elementType: {
+      type: String,
+      required: true,
+      default: 'OOO' // unset: out of order
+    }
+  }
+}
+</script>
+<style lang="scss">
+@import "assets/app";
+.element {
+    $height: 37px;
+    $width: 119px;
+    $border: 3px solid $color-border;
+    @mixin elm-font {
+        color: $color-base;
+        letter-spacing: 1px;
+        font-size: 23px;
+        font-weight: bold;
+        font-family: Arial;
+    }
+    &__frame {
+        width: $width;
+        height: $height;
+        display: flex;
+        outline: $border;
+        background-color: $color-base;
+    }
+    &__input {
+        width: $width * (1 /10);
+        height: $height / 2;
+        &--border-bottom {
+            border-bottom: $border;
+        }
+    }
+    &__output {
+        width: $width * (1 /10);
+        height: $height;
+    }
+    &__center {
+        width: $width * (8 / 10);
+        height: $height;
+        background-color: $color-border;
+        @include elm-font;
+        @include text-to-center($height);
+    }
+}
+</style>
