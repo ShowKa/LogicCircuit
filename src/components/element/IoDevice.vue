d283928f1f76deff4151d88d052a4b7492814aa8
Shota kanamoto <kanamoto.official@gmail.com>
2020-02-23 00:30:39 +0900

    Feature screen ic (#6)
    * Design Element For LogicCircuit
    
    * enable dragging element
    
    * remove lint rules that is imcompatible with editor's formatter
    
    * Bit element
    
    * IoDevice...

---
+++ src/components/element/IoDevice.vue
@@ -0,0 +1,25 @@
+
+<template>
+<div
+  class="io-device"
+  @mouseup="toggleStatus"
+/>
+</template>
+<script>
+export default {
+  props: {
+    ioType: {
+      type: String,
+      required: true
+    }
+  },
+  data: () => ({
+    status: false
+  }),
+  methods: {
+    toggleStatus: function(e) {
+      this.status = !this.status
+    }
+  }
+}
+</script>
