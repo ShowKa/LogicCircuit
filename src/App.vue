9a293299e295d32556e0c4e21877bc4c2c17d356
John Doe <johndoe@example.com>
2020-03-18 00:43:47 +0900

    add component: Debug

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,27 @@
 <template>
-  <field />
+<div class="app">
+  <Debug v-if="isDebugMode" />
+  <Field />
+</div>
 </template>
 
 <script>
 import Field from './components/Field.vue'
+import Debug from './Debug.vue'
 export default {
   components: {
-    Field
+    Field,
+    Debug
+  },
+  computed: {
+    isDebugMode() {
+      return process.env.NODE_ENV === 'development'
+    }
   }
 }
 </script>
+<style lang="scss">
+.app {
+    height: 100%;
+}
+</style>
